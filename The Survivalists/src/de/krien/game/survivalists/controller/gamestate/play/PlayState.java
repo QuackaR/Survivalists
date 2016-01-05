ed4e4b2a1ef54af9383ff215d7fc8b733bf7d804
Alex <alex@krien.name>
2016-01-06 00:07:53 +0100

    Added Player Object

--- The Survivalists/src/de/krien/game/survivalists/controller/gamestate/play/PlayState.java
+++ The Survivalists/src/de/krien/game/survivalists/controller/gamestate/play/PlayState.java
@@ -2,45 +2,82 @@ package de.krien.game.survivalists.controller.gamestate.play;
 
 import java.util.List;
 
+import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
+
 import de.krien.game.survivalists.controller.gamestate.IGameState;
 import de.krien.game.survivalists.controller.input.InputHandler;
+import de.krien.game.survivalists.model.player.Player;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
+import javafx.scene.transform.Rotate;
 
 public enum PlayState implements IGameState {
 
 	INSTANCE;
 
-	public int pos_X = 50;
-	public int pos_Y = 50;
-
-	public int width = 25;
-	public int height = 25;
+	private Player player = new Player();
 	
-	public int movementSpeed = 5;
-
 	public void draw(GraphicsContext graphicalContext) {
-		graphicalContext.fillOval(pos_X, pos_Y, width, height);
+		graphicalContext.drawImage(
+				player.getImage(), 
+				player.getPosition().getX(), 
+				player.getPosition().getY(),
+				player.getSize().getX(),
+				player.getSize().getY()
+		);
+		
+//		graphicalContext.fillOval(
+//				player.getPosition().getX(), 
+//				player.getPosition().getY(), 
+//				player.getSize().getX(), 
+//				player.getSize().getY()
+//		);
 	}
 
 	public void update(float secondsElapsed) {
+		handleInput();
+	}
+
+	//TODO Create new class and extract method - PlayStateInputHandler
+	//TODO Refactor code
+	private void handleInput() {
 		List<KeyCode> pressedKeys = InputHandler.INSTANCE.getKeyPressedList();
-		
 		if (pressedKeys.contains(KeyCode.D)) {
-			pos_X+=movementSpeed;
+			player.setPosition(player.getPosition().add(new Vector2D(player.getMovementSpeed(), 0)));
 		}
 
 		if (pressedKeys.contains(KeyCode.A)) {
-			pos_X-=movementSpeed;
+			player.setPosition(player.getPosition().subtract(new Vector2D(player.getMovementSpeed(), 0)));
 		}
 
 		if (pressedKeys.contains(KeyCode.S)) {
-			pos_Y+=movementSpeed;
+			player.setPosition(player.getPosition().add(new Vector2D(0, player.getMovementSpeed())));
 		}
 
 		if (pressedKeys.contains(KeyCode.W)) {
-			pos_Y-=movementSpeed;
+			player.setPosition(player.getPosition().subtract(new Vector2D(0, player.getMovementSpeed())));
 		}
-
+	}
+	
+	private void rotateImage(String direction) {
+		final ImageView imageView = new ImageView(player.getImage());
+	    imageView.setTranslateZ(imageView.getBoundsInLocal().getWidth() / 2.0);
+	    imageView.setRotationAxis(Rotate.Y_AXIS);
+	    
+		switch(direction) {
+		case "up":
+			imageView.setRotate(270);
+			break;
+		case "down":
+			imageView.setRotate(90);
+			break;
+		case "left":
+			imageView.setRotate(180);
+			break;
+		case "right":
+			imageView.setRotate(0);
+			break;
+		}
 	}
 }
--- The Survivalists/src/de/krien/game/survivalists/model/player/Player.java
+++ The Survivalists/src/de/krien/game/survivalists/model/player/Player.java
@@ -1,7 +1,68 @@
 package de.krien.game.survivalists.model.player;
 
+import org.apache.commons.math3.geometry.euclidean.twod.Vector2D;
+
+import javafx.scene.image.Image;
+
 public class Player {
 
-	
-	
+	private final int DEFAULT_POSITION = 50; // Temporary
+	private final int DEFAULT_SIZE = 75;
+	private final int DEFAULT_MOVEMENT_SIZE = 5;
+	private final String DEFAULT_IMAGE = "res/player.jpg";
+
+	private Vector2D position;
+	private Vector2D size;
+	private int movementSpeed;
+
+	private Image image;
+
+	public Player() {
+		super();
+		this.position = new Vector2D(DEFAULT_POSITION, DEFAULT_POSITION);
+		this.size = new Vector2D(DEFAULT_SIZE, DEFAULT_SIZE);
+		this.movementSpeed = DEFAULT_MOVEMENT_SIZE;
+		this.image = new Image(DEFAULT_IMAGE);
+	}
+
+	public Player(Vector2D position, Vector2D size, int movementSpeed, Image image) {
+		super();
+		this.position = position;
+		this.size = size;
+		this.movementSpeed = movementSpeed;
+		this.image = image;
+	}
+
+	public Vector2D getPosition() {
+		return position;
+	}
+
+	public void setPosition(Vector2D position) {
+		this.position = position;
+	}
+
+	public Vector2D getSize() {
+		return size;
+	}
+
+	public void setSize(Vector2D size) {
+		this.size = size;
+	}
+
+	public int getMovementSpeed() {
+		return movementSpeed;
+	}
+
+	public void setMovementSpeed(int movementSpeed) {
+		this.movementSpeed = movementSpeed;
+	}
+
+	public Image getImage() {
+		return image;
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
+
 }
